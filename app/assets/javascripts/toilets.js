effa7248df6aae0cc7b52e4fd2954824b751c79a
Melissa Miller <lis.d.miller@gmail.com>
2016-04-05 13:22:14 -0400

    Gets angular to talk to railsBANG WooBANG

--- app/assets/javascripts/toilets.js
+++ app/assets/javascripts/toilets.js
@@ -1,8 +1,33 @@
+//= require angular
+//= require angular-resource
+
 "use strict";
 
 (function() {
   angular
-  .module("toilets", [])
-  .controller("toilet_controller",)
+  .module("toilets", [
+    "ngResource"
+  ])
+  .controller("tcontroller", [
+    "$resource",
+    ToiletsController
+  ]);
 
+  function ToiletsController($resource) {
+    var vm = this;
+    var Toilet = $resource("/toilets/:id.json", {}, {
+      update: {method: "PUT"}
+    });
+    vm.data = Toilet.query();
+
+    vm.destroy = function(toilet_index) {
+      vm.data.splice(product_index, 1);
+    } // end destroy
+
+    vm.new_toilet = {};
+    vm.create = function() {
+      vm.data.push(angular.copy(vm.new_toilet));
+      vm.new_toilet = {};
+    }
+  }
 })();
