a05b3b0b2f742ff23b6936f154969ecfcc0950d1
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-04-06 15:14:08 -0400

    adds map function to factory

--- app/assets/javascripts/toilets.js
+++ app/assets/javascripts/toilets.js
@@ -53,13 +53,51 @@
         update: {method: "PUT"}
       });
       Toilet.all = Toilet.query();
+      Toilet.createMarker = function(info, myMap){
+        var markers = [];
+        var infoWindow = new google.maps.InfoWindow();
+        var geocoder = new google.maps.Geocoder();
+        var address = info.address;
+        geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            var marker = new google.maps.Marker({
+              map: myMap,
+              position: results[0].geometry.location,
+              title: info.city
+            });
+            marker.content = '<div class="infoWindowContent"><a href="http://starbucks.com">' + info.desc + '</a></div>';
+            google.maps.event.addListener(marker, 'click', function(){
+              infoWindow.setContent('<h2>' + info.name + '</h2>' + marker.content);
+              infoWindow.open(myMap, marker);
+            });
+            markers.push(marker);
+          }
+        });
+      };
       return Toilet;
   } // end toiletFactoryFunction
 
   function indexCtrlFunction(Toilet) {
     var indexVM = this;
     indexVM.toilets = Toilet.all;
-    indexVM.new_toilet = new Toilet;
+    var mapOptions = {
+      zoom: 4,
+      center: new google.maps.LatLng(38.90529, -77.02489),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var myMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+    indexVM.toilets.$promise.then(function(response){
+      for (i = 0; i < indexVM.toilets.length; i++){
+        Toilet.createMarker(indexVM.toilets[i], myMap);
+      }
+    });
+
+    // creates the markers' info windows
+    openInfoWindow = function(e, selectedMarker){
+      e.preventDefault();
+      google.maps.event.trigger(selectedMarker, 'click');
+    };
+    indexVM.new_toilet = new Toilet();
   } // end indexCtrlFunction
 
 
@@ -78,13 +116,14 @@
     return {
       templateUrl: "partials/toilet.form.html",
       scope: {
-        destination: "=",
+        toilet: "=",
         formMethod: "@"
       },
       link: function(scope) {
         Toilet.create = function() {
           Toilet.save(scope.toilet, function(response) {
             Toilet.all.push(response);
+            Toilet.createMarker(response);
           }); // end of save
         } // end of create function
       } // end of link function
