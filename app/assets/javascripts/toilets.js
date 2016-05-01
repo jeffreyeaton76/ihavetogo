122b76f142757c20c0ee9de992c1dd953b2b563f
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-05-01 13:04:41 -0400

    fixes link in markers

--- app/assets/javascripts/toilets.js
+++ app/assets/javascripts/toilets.js
@@ -92,7 +92,7 @@
             position: results[0].geometry.location,
             title: info.business_name
           });
-          marker.content = '<div class="infoWindowContent"><a href="#/toilet/' + info.id + '">' + 'View Toilet!</a></div>';
+          marker.content = '<div class="infoWindowContent"><a href="toilet/' + info.id + '">' + 'View Toilet!</a></div>';
           google.maps.event.addListener(marker, 'click', function(){
             infoWindow.setContent(info.business_name + marker.content);
             infoWindow.open(myMap, marker);
